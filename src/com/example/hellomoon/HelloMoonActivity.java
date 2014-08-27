2e6ace7b6fb6fccf0eb29c4dba94edfa823518e1
grpp <nat_hanlaw@hotmail.com>
2014-08-26 22:06:10 -0400

    Initial

---
+++ src/com/example/hellomoon/HelloMoonActivity.java
@@ -0,0 +1,16 @@
+package com.example.hellomoon;
+
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class HelloMoonActivity extends ActionBarActivity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_hello_moon);
+	}
+
+}
